a55f839f91e48c40d06cde354293cb75f8c18d08
Sandeepbh07 <cheetrirocker07@gmail.com>
2022-06-04 16:55:11 +0545

    create search header options component

--- components/SearchHeader.js
+++ components/SearchHeader.js
@@ -3,6 +3,7 @@ import { useRouter } from "next/router";
 import { useRef } from "react";
 import { MicrophoneIcon, SearchIcon, XIcon } from "@heroicons/react/solid";
 import User from "./User";
+import SearchHeaderOptions from "./SearchHeaderOptions";
 
 const SearchHeader = () => {
   const router = useRouter();
@@ -12,7 +13,7 @@ const SearchHeader = () => {
     e.preventDefault();
     const term = searchInputRef.current.value;
     if (!term.trim()) return;
-    router.push(`/search?term=${term.trim()}`);
+    router.push(`/search?term=${term.trim()}&searchType=`);
   };
   return (
     <header className="sticky top-0 bg-white">
@@ -42,6 +43,7 @@ const SearchHeader = () => {
         </form>
         <User className="ml-auto whitespace-nowrap" />
       </div>
+      <SearchHeaderOptions />
     </header>
   );
 };
