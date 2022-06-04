1dc619994293ff76d9fb5e3382f62448097ce738
Sandeepbh07 <cheetrirocker07@gmail.com>
2022-06-04 16:09:57 +0545

    create search header component

--- components/User.js
+++ components/User.js
@@ -1,6 +1,6 @@
 import { useSession, signIn, signOut } from "next-auth/react";
 
-const User = () => {
+const User = ({ className }) => {
   const { data: session } = useSession();
   if (session) {
     return (
@@ -9,7 +9,7 @@ const User = () => {
           src={session.user.image}
           alt="pro-pic"
           onClick={signOut}
-          className="h-10 w-10 rounded-full hover:bg-gray-200 hover:cursor-pointer p-1"
+          className={`h-10 w-10 rounded-full hover:bg-gray-200 hover:cursor-pointer p-1 ${className}`}
         />
       </>
     );
@@ -18,7 +18,7 @@ const User = () => {
     <>
       <button
         onClick={signIn}
-        className="bg-blue-500 text-white px-6 py-2 font-medium rounded-md hover:brightness-105 hover:shadow-md"
+        className={`bg-blue-500 text-white px-6 py-2 font-medium rounded-md hover:brightness-105 hover:shadow-md ${className}`}
       >
         Sign In
       </button>
